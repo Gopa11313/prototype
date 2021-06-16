e60526f664669800133ded519c9e39feb4081ffb
Gopa11313 <gopalaa9819525993@gmail.com>
2021-06-16 11:30:30 +0545

    simple chnages

--- src/HomepageComponents/OurServices.js
+++ src/HomepageComponents/OurServices.js
@@ -43,15 +43,60 @@ export class OurServices extends Component {
                             niloofar.com</p>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-3">
-                        <Button className="p-2" style={{ "background-image": "linear-gradient(255deg,  #DC143C 0%,#ff4e00 100%)", "border": "	#DC143C 1px solid", width: '100%' }}>APP STORE</Button>
+
+
+                <div>
+                    <div className="row my-3 mt-5">
+                        <div className="col-4" >
+                            <div className="text-center shadow-lg mb-2 bg-white rounded" style={{ backgroundColor: "#F5F5F5", height: "100%", width: "100%" }}>
+                                <svg className="my-3" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="red" className="bi bi-trophy-fill" viewBox="0 0 18 16">
+                                    <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z" />
+                                </svg>
+                                <p>AWARDS</p>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="text-center shadow-lg p-1 mb-2 bg-white rounded" style={{ backgroundColor: "#F5F5F5", height: "250px", width: "100%" }}>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="text-center shadow-lg p-1 mb-2 bg-white rounded" style={{ backgroundColor: "#F5F5F5", height: "250px", width: "100%" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="red" class="bi bi-megaphone-fill" viewBox="0 0 18 16">
+                                    <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0v-11zm-1 .724c-2.067.95-4.539 1.481-7 1.656v6.237a25.222 25.222 0 0 1 1.088.085c2.053.204 4.038.668 5.912 1.56V3.224zm-8 7.841V4.934c-.68.027-1.399.043-2.008.053A2.02 2.02 0 0 0 0 7v2c0 1.106.896 1.996 1.994 2.009a68.14 68.14 0 0 1 .496.008 64 64 0 0 1 1.51.048zm1.39 1.081c.285.021.569.047.85.078l.253 1.69a1 1 0 0 1-.983 1.187h-.548a1 1 0 0 1-.916-.599l-1.314-2.48a65.81 65.81 0 0 1 1.692.064c.327.017.65.037.966.06z" />
+                                </svg>
+                                <p>SOCIAL MARKETING</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-3" >
-                        <Button variant="outline-dark" className="ml-4" style={{ width: '100%' }}>GOOGLE PLAY</Button>
+                    <div className="row">
+                        <div className="col-4">
+                            <div className="text-center shadow-lg p-1 mb-3 bg-white rounded" style={{ backgroundColor: "#F5F5F5", height: "250px", width: "100%" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="red" className="bi bi-brush-fill" viewBox="0 0 18 16">
+                                    <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04z" />
+                                </svg>
+                                <p>DESIGNING</p>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="text-center shadow-lg p-1 mb-3 bg-white rounded" style={{ backgroundColor: "#F5F5F5", height: "250px", width: "100%" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="red" class="bi bi-tools" viewBox="0 0 18 16">
+                                    <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.356 3.356a1 1 0 0 0 1.414 0l1.586-1.586a1 1 0 0 0 0-1.414l-3.356-3.356a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0zm9.646 10.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708zM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11z" />
+                                </svg>
+                                <p>DEVELOPMENT</p>
+                            </div>
+
+
+                        </div>
+                        <div className="col-4">
+                            <div className="text-center shadow-lg p-1 mb-3 bg-white rounded " style={{ backgroundColor: "#F5F5F5", height: "250px", width: "100%" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="red" className="bi bi-book-fill" viewBox="0 0 16 16">
+                                    <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                                </svg>
+                                <p>CONTENT WRITING</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </React.Fragment>
         )
     }
